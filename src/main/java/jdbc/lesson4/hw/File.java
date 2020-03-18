a781faff755a20a9b630f3aeddf07414c07015d4
Lenovo T540p <sonychkalapochka94@gmail.com>
2020-03-18 23:31:17 +0200

    jdbc les 4 jdbc without lombok

--- src/main/java/jdbc/lesson4/hw/File.java
+++ src/main/java/jdbc/lesson4/hw/File.java
@@ -1,10 +1,10 @@
 package jdbc.lesson4.hw;
 import lombok.*;
 
-@Getter
+/*@Getter
 @Setter
 @ToString
-@EqualsAndHashCode
+@EqualsAndHashCode*/
  public class File {
     private long id;
     private String name;
@@ -20,5 +20,62 @@ import lombok.*;
         this.size = size;
         this.storage = storage;
     }
+    public long getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public Storage getStorage() {
+        return storage;
+    }
+
+    public void setStorage(Storage storage) {
+        this.storage = storage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        File file = (File) o;
+
+        if (id != file.id) return false;
+        if (size != file.size) return false;
+        if (!name.equals(file.name)) return false;
+        if (!format.equals(file.format)) return false;
+        return storage.equals(file.storage);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + name.hashCode();
+        result = 31 * result + format.hashCode();
+        result = 31 * result + (int) (size ^ (size >>> 32));
+        result = 31 * result + storage.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "File{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", format='" + format + '\'' +
+                ", size=" + size +
+                ", storage=" + storage +
+                '}';
+    }
 }
--- src/main/java/jdbc/lesson4/hw/Storage.java
+++ src/main/java/jdbc/lesson4/hw/Storage.java
@@ -1,10 +1,12 @@
 package jdbc.lesson4.hw;
 import lombok.*;
 
-@Getter
+import java.util.Arrays;
+
+/*@Getter
 @Setter
 @ToString
-@EqualsAndHashCode
+@EqualsAndHashCode*/
 public class Storage {
 
     private long id;
@@ -18,5 +20,52 @@ public class Storage {
         this.storageCountry = storageCountry;
         this.storageMaxSize = storageMaxSize;
     }
+    public long getId() {
+        return id;
+    }
 
+    public String[] getFormatsSupported() {
+        return formatsSupported;
+    }
+
+    public String getStorageCountry() {
+        return storageCountry;
+    }
+
+    public long getStorageMaxSize() {
+        return storageMaxSize;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Storage storage = (Storage) o;
+
+        if (id != storage.id) return false;
+        if (storageMaxSize != storage.storageMaxSize) return false;
+        // Probably incorrect - comparing Object[] arrays with Arrays.equals
+        if (!Arrays.equals(formatsSupported, storage.formatsSupported)) return false;
+        return storageCountry.equals(storage.storageCountry);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + Arrays.hashCode(formatsSupported);
+        result = 31 * result + storageCountry.hashCode();
+        result = 31 * result + (int) (storageMaxSize ^ (storageMaxSize >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Storage{" +
+                "id=" + id +
+                ", formatsSupported=" + Arrays.toString(formatsSupported) +
+                ", storageCountry='" + storageCountry + '\'' +
+                ", storageMaxSize=" + storageMaxSize +
+                '}';
+    }
 }
